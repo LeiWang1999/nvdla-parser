ececc8d46fe4504879abf239e041140c0a9e6b8b
LeiWang1999 <2230647190@qq.com>
2021-04-17 19:25:55 +0800

    remove useless code

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,11 +1,6 @@
 #include "tools.h"
-#include <random>
-#include <ctime>
 #include <unistd.h>
-#include <dirent.h>
-#include <ctime>
 
-using std::istringstream;
 using std::string;
 
 
@@ -14,14 +9,4 @@ int is_file_exists(const string &path)
     return !access(path.c_str(), F_OK);
 }
 
-int is_folder_exist(const string &path)
-{
-    DIR *dp;
-    if ((dp = opendir(path.c_str())) == NULL)
-    {
-        return 0;
-    }
 
-    closedir(dp);
-    return -1;
-}
